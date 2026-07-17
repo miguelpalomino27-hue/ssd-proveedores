a80a812f034c81863fff689cb832ab7289e7e96c
MIGUEL MATHEOS PALOMINO CARDENAS <miguel.palomino.27@unsch.edu.pe>
2026-07-16 21:18:22 -0500

    fix: variables db correctas

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
